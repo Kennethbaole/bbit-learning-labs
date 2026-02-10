5eb9b0022b2f8180b2364a0d8d32401d1fee832a
Kenneth Le <kennethble@gmail.com>
2026-02-10 22:37:54 +0000

    Implement producer section

--- tech_lab_on_campus/market_watch/producer_and_consumer/producer/solution/producer_sol.py
+++ tech_lab_on_campus/market_watch/producer_and_consumer/producer/solution/producer_sol.py
@@ -11,17 +11,16 @@ setupRMQConnection Function: Establish connection to the RabbitMQ service.
 publishOrder: Publish a simple UTF-8 string message from the parameter.
 publishOrder: Close Channel and Connection.
 '''
+import os
 import pika
-from ..producer_interface import mqProducerInterface
+from producer_interface import mqProducerInterface
 
 class mqProducer(mqProducerInterface):
     def __init__(self, routing_key: str, exchange_name: str) -> None:
         self.routing_key = routing_key
         self.exchange_name = exchange_name
-
         self.connection = None
         self.channel = None
-
         self.setupRMQConnection()
 
     def setupRMQConnection(self) -> None:
